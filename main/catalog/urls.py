2579480eb8a6763224231b247f7d474ecdeb6bd1
Vladaf <vladaf1999@tut.by>
2021-08-31 11:19:24 +0300

    artists

--- main/catalog/urls.py
+++ main/catalog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("browse/", views.browse, name="browse_page"),
     path("charts/", views.charts, name="charts_page"),
     path("artists/", views.artists, name="artists_page"),
+    path("artists/<artist_name>/", views.artists_detail, name="artists_detail_page"),
     path("", lambda request: redirect(reverse("discover_page"))),
 ]
--- main/catalog/views.py
+++ main/catalog/views.py
@@ -28,6 +28,21 @@ def charts(request):
 
 def artists(request):
     artist_list = User.objects.filter(is_musician = True)
-    track_list = Song.objects.filter(band = artist_list)
-    context = {"artist_list": artist_list}
+    counter = []
+    for artist in artist_list:
+        track_list = Song.objects.filter(band = artist)
+        count = len(track_list)
+        counter.append(count)
+    artist_info = zip(artist_list, counter)
+    context = {"artist_info": artist_info}
     return render(request, "artists.html", context)
+
+def artists_detail(request, artist_name):
+    #users_list = User.objects.filter(is_musician = True)
+    artists_detail = User.objects.get(username = artist_name)
+    if artists_detail:
+        music_detail = Song.objects.filter(band = artist_name)
+        context = {"artists_detail": artists_detail, "music_detail": music_detail}
+        return render(request, "artists_detail.html", context)
+    else:
+        return redirect(reverse("home_page"))
